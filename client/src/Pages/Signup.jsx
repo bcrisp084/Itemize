ebf9b48c2eec6a66a0dee16323f3f12fd6ab7a01
Brian Crisp <bcrisp084@yahoo.com>
2023-09-30 20:57:27 -0400

    removed vite files/added signup component

---
+++ client/src/Pages/Signup.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router";
+
+const Signup = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <button>Sign In</button>
+    </div>
+  );
+};
+
+export default Signup;
