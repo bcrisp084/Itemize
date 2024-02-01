eb66a4aead3a83e16fcb6cdac8d5bfedacd1447c
Brian Crisp <bcrisp084@yahoo.com>
2024-02-01 12:33:29 -0500

    created the models and schema files

---
+++ server/schemas/index.js
@@ -0,0 +1,4 @@
+const typeDefs = require("./typeDefs");
+const resolvers = requrie("./resolvers.js");
+
+module.exports = { typeDefs, resolvers };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
-const { typeDefs, resolvers } = require("./schemas");
+const PORT = process.env.PORT || 3000;
+// const { typeDefs, resolvers } = require("./schemas");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
