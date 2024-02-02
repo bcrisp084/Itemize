e6d3f0698a3cf81e691b69324b803fd7eb61cb22
Brian Crisp <bcrisp084@yahoo.com>
2024-02-02 01:34:39 -0500

    added chat section to the home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import todoImage from "../Assets/todo.jpg";
+import chatImage from "../Assets/chat.jpg";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,6 +20,7 @@ const Home = () => {
       <Box id={"Features"} className="features" sx={{ flexGrow: 1 }}>
         <Grid
           container
+          id="feature-grid"
           justifyContent={"center"}
           alignItems={"center"}
           rowSpacing={1}
@@ -34,10 +36,9 @@ const Home = () => {
             </div>
             <div>
               <p className="decleration">
-                Itemize is a simple and easy to use task manager. It allows you
-                to create and manage tasks, set deadlines, and keep track of
-                your progress. Its perfect for keeping track of your daily
-                tasks, or for managing larger projects.
+                Itemize is a simple and easy to use task manager. Its perfect
+                for keeping track of your daily tasks, or for managing larger
+                projects.
               </p>
             </div>
           </Grid>
@@ -50,28 +51,44 @@ const Home = () => {
       </Box>
       <Box id={"Customers"} sx={{ flexGrow: 1 }}>
         <Grid
+          id="customer-grid"
           container
           justifyContent={"center"}
           alignItems={"center"}
           rowSpacing={1}
-          height={"calc(100vh - 300px)"}
+          height={"calc(100vh - 80px)"}
           columnSpacing={{ md: 3 }}
         >
           <Grid xs={9} md={5} xl={4}>
-            <Item>xs=8</Item>
+            <div className="chat-div">
+              <img id="chat-image" src={chatImage} />
+            </div>
           </Grid>
           <Grid xs={9} md={5} xl={4}>
-            <Item>xs=8</Item>
+            <div className="chat-statement">
+              <h2>
+                Chat with your <span className="team">team</span> while you
+                work.
+              </h2>
+            </div>
+            <div>
+              <p className="chat-decleration">
+                Itemize is a simple and easy to use chat application. Its
+                perfect for keeping track of your daily tasks, or for managing
+                larger projects.
+              </p>
+            </div>
           </Grid>
         </Grid>
       </Box>
       <Box id={"Integrations"} sx={{ flexGrow: 1 }}>
         <Grid
+          id="integration-grid"
           container
           justifyContent={"center"}
           alignItems={"center"}
           rowSpacing={1}
-          height={"calc(100vh - 300px)"}
+          height={"calc(100vh - 80px)"}
           columnSpacing={{ md: 3 }}
         >
           <Grid xs={9} md={5} xl={4}>
